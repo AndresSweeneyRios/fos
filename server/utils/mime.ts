f91e1451960cc037176980b8d46de1d3dcfc67ec
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-04-28 15:14:37 -0700

    Removed unused utils

--- server/utils/mime.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs"
-import path from "path"
-
-const mimetypes = fs.readFileSync(
-  path.join(__dirname, "mimetypes.json"), 
-  "utf8",
-)
-
-export default (extension: string): string => mimetypes[extension]
